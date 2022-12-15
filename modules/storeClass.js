af7e46e743ba7cd1e41014841c798a40bfe7e19e
gijoe2day <gjuliao32@hotmail.com>
2022-12-15 17:45:52 -0500

    added remove and sort function

--- modules/storeClass.js
+++ modules/storeClass.js
@@ -1,10 +1,11 @@
+import UI from "./uiClass.js";
+
 export default class Store {
     static saveTaskLocalStorage = (tasks) => {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
+       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
 
     static getLocalStorage = () => {
-        
        let tasks = localStorage.getItem('tasks');
        if(tasks) {
         tasks = JSON.parse(tasks);
@@ -12,4 +13,23 @@ export default class Store {
        }
        return [];
     }
+
+    static removeItem(el, index) {
+        let tasks = this.getLocalStorage();
+        tasks.splice(index, 1);
+        tasks.forEach((el, index) => {el.index = index});
+        this.saveTaskLocalStorage(tasks);
+        UI.displayTask(tasks)
+    }
+
+    static editItem(description, index) {
+        console.log(index);
+        for (let i = 0; i < this.getLocalStorage().length; i++) {
+            if (this.getLocalStorage()[i].index == index) {
+                this.getLocalStorage()[i].description = el.description;
+        }
+
+    }
+
+}
 }
--- modules/uiClass.js
+++ modules/uiClass.js
@@ -21,20 +21,41 @@ export default class UI {
 
         tasks.forEach((el) => {
             let element = document.createElement('li');
-            element.innerText = el.description + ' ' + el.index;
+            element.innerHTML = `
+            <input type="text" id="taskItem" class="description" value="${el.description}" />
+            <i class="fa-solid fa-trash-can"></i>
+            <i class="fa-solid fa-pen-to-square"></i>
+            `;
             ul.appendChild(element);
+        });
+
+        const removeTask = document.querySelectorAll('.fa-trash-can');
+        removeTask.forEach((el, index) => {
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                Store.removeItem(el, index)
+            })
+        })
+
+        const editTask = document.querySelectorAll('.fa-pen-to-square');
+        editTask.forEach((el, index) => {
+            const taskItem = document.querySelectorAll('#taskItem');
+            el.addEventListener('click', (e) => {
+                e.preventDefault();
+                console.log(taskItem, index);
+                Store.editItem(el.parentElement, index);
+        })
         })
     }
-
+    
     static newTask = (description) => {
         let tasks = Store.getLocalStorage('tasks');
         let index = tasks.length;
         
         const task = new Task(index, description);
         tasks.push(task);
+        Store.saveTaskLocalStorage(tasks);
         UI.displayTask(tasks);
-        
-       Store.saveTaskLocalStorage(tasks);
     }
 }
 
